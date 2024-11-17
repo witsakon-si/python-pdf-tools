16cde4bc080390edd9ade3cd5dc1669689f16076
wissakorn <witsakon.si@gmail.com>
2024-11-17 20:18:55 +0700

    update

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from streamlit_option_menu import option_menu
 from views import menu_pdf, menu_image, about_me
 
 with st.sidebar:
-    selected = option_menu("PDF/Image Tools", ["PDF", "Image", "About Me"], 
+    selected = option_menu("PDF/Image Tools", ["PDF", "Image", "About Me"],
         icons=['file-earmark-pdf-fill', 'image-fill', 'question-diamond-fill'], menu_icon="tools", default_index=0)
 
 if selected == "PDF":
@@ -12,3 +12,4 @@ elif selected == "Image":
     menu_image.create_page()
 else:
     about_me.create_page()
+
